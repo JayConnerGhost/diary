b984c99d0d66efc3d74810fb5ed653be3e675e2d
digitalBitch <ghosttk@protonmail.com>
2018-01-06 15:24:48 +0000

    adding test framework and test project

---
+++ DiaryWeb.Tests/WebApplicationLayoutSpecifications.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DiaryWeb.Tests
+{
+    public class WebApplicationLayoutSpecifications
+    {
+        [Fact]
+        public void ApplicationName_should_be_diary()
+        {
+            Assert.True(false);
+        }
+    }
+}
