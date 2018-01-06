e835b90b4423e57427c9bb81d89c81207601f4ca
digitalBitch <ghosttk@protonmail.com>
2018-01-06 15:08:29 +0000

    initial check in with asp mvc base configured

---
+++ DiaryWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DiaryWeb.Startup))]
+namespace DiaryWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
